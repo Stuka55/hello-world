614461875bac4b0b2e96e9a245fc1c9cd9efce4c
Stuka55 <63323662+Stuka55@users.noreply.github.com>
2020-08-19 16:28:51 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
+Test using GITHUB
+// Author Your Name
+// Description: Hello World in C++
+//============================================================
+main
 #include <iostream>
-
+#include <string>
 using namespace std;
-
+int hello1Function(string name1);
+int hello2Function(string name2);
 int main()
 {
-  
-cout << "Hello Everybody!;
-
-
-
+string name1="BOB";
+hello1Function(name1);
+string name2="Phil";
+hello2Function(name2);
 return 0;
 }
+
+//hello1Function
+/*
+* hello1Function.cpp
+*
+*/
+#include <iostream>
+using namespace std;
+int hello1Function(string name1)
+{
+cout << "hello World " << name1 << endl;
+return 0;
+}
+// hello1Function
